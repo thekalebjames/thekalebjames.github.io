818133241d03743302fd3e45e18283e0e72a0c52
thekalebjames <111808398+thekalebjames@users.noreply.github.com>
2023-05-05 00:50:32 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿@import url("https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap");
+body{
+    margin: 0px auto;
+    width: 100%;
+}
+.topnav{
+    background-color: rgba(15, 7, 161, 0.699);
+    overflow: hidden;
+    position: fixed;
+    float: none;
+    width:  100%;
+}
+
+/* style the link inside the navigation bar */
+.topnav a{
+    float: left;
+    justify-content: center;
+    display: block;
+    color: #f2f2f2;
+    padding: 14px 16px;
+    font-size: 18px;
+    text-shadow: 3px 3px 3px rgb(22,22,22);
+}
